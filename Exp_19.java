9c5c7b60c9e095ea739b16e4aaad96d5224197d7
octocat <105317263+octocn@users.noreply.github.com>
2024-02-21 02:26:42 -0800

    Update Exp_19.java

--- Exp_19.java
+++ Exp_19.java
@@ -1,19 +1,19 @@
-import java.io.*;
 import java.util.Scanner;
-public class Exception{
-  public static void main(String[] args){
-    Scanner sc = new Scanner();
-    System.out.println("Enter a number: ");
-    try{
-      int num = sc.nextInt();
-      int result = 100/num;
-      System.out.println("Result: "+result);
-    }catch(ArithmeticException e){
-      System.out.println("Error: "+e.getMessage());
-    }catch(Exception e){
-      System.out.println("Error: "+e.getMessage());
-    }finally{
-      sc.close();
+
+public class Exec1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        try {
+            int num = sc.nextInt();
+            int result = 100 / num;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (Throwable e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            sc.close();
+        }
     }
-  }
 }
