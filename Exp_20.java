7d3a4b82e3419f976dd9b17386b2c0a0e31b16c1
octocat <105317263+octocn@users.noreply.github.com>
2024-02-21 02:49:50 -0800

    Update Exp_20.java

--- Exp_20.java
+++ Exp_20.java
@@ -1,18 +1,33 @@
 import java.util.Scanner;
-class DivideExceptionD extends RuntimeException{
-  public DivideException(String message){
-    super(message);
-  }
+class DivideException extends RuntimeException{
+	private String message;
+	public DivideException(String message){
+		super(message);
+		this.message = message;
+	}
 }
 public class UserExec{
-  public static void main(String[] args){
-    try(Scanner sc = new Scanner(System.in)){
-      int dividend = getValidInput("Enter dividend: ", sc);
-      int divisor = getValidInput("Enter divisor: ",sc);
-      if (divisor == 0){
-        throw new DivideException("Cannot divide by zero");
-      }
-      double result = (double) dividend/divisor;
-      System.out.printf("Result: %.2f%n",result);
-    }catch
-        
+	public static void main(String args[]){
+		try(Scanner sc = new Scanner(System.in)){
+			int dividend = getValidInput("Enter Dividend: ", sc);
+			int divisor = getValidInput("Enter Divisor: ", sc);
+		if (divisor == 0){
+			throw new DivideException("Cannot Divide by zero!");
+		}
+		double result = (double) dividend/divisor;
+		System.out.printf("Result: %.2f%n", result);
+		}catch(DivideException e){
+			System.err.println("Error: "+e.getMessage());
+		}catch(ArithmeticException e){
+			System.err.println("Error: Division by zero!");
+	}
+}
+private static int getValidInput(String message, Scanner sc){
+		System.out.println(message);
+		while(!sc.hasNextInt()){
+			System.out.println("Invalid!");
+			sc.next();
+		}
+		return sc.nextInt();
+}
+}
